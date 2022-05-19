89f179c6ae6f4333c11789ac56f284ea5a2bc367
강석현 <tjrgus0713@gmail.com>
2022-05-19 16:09:49 +0900

    ⚡️:: (#8) follow api 추가

---
+++ src/models/follow/response.ts
@@ -0,0 +1,14 @@
+import { TSchool, TUser } from '../../utils/interface/common';
+
+export interface IGetFollowingListResponse {
+  follower_user_list: Array<{
+    user_id: number;
+    name: string;
+    profile_image_url: string;
+    user_scope: TUser;
+    school: TSchool;
+    is_follow: boolean;
+  }>;
+}
+
+export interface IGetFollowerListResponse extends IGetFollowingListResponse {}
