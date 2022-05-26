226bec679dc60476e81a1697d9fb47b6c3e91c7b
강석현 <tjrgus0713@gmail.com>
2022-05-26 09:25:48 +0900

    ♻️:: (#14) 팔로우 경로수정

--- src/models/follow/response.ts
+++ src/models/follow/response.ts
@@ -1,14 +1,18 @@
 import { TSchool, TUser } from '../common';
 
-export interface IGetFollowingListResponse {
-  follower_user_list: Array<{
-    user_id: number;
-    name: string;
-    profile_image_url: string;
-    user_scope: TUser;
-    school: TSchool;
-    is_follow: boolean;
-  }>;
+export interface IFollow {
+  user_id: number;
+  name: string;
+  profile_image_url: string;
+  user_scope: TUser;
+  school: TSchool;
+  is_follow: boolean;
 }
 
-export interface IGetFollowerListResponse extends IGetFollowingListResponse {}
+export interface IGetFollowingListResponse {
+  following_user_list: IFollow[];
+}
+
+export interface IGetFollowerListResponse {
+  follower_user_list: IFollow[];
+}
