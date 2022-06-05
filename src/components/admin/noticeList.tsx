1d041bd1666554995b1737f0d2b072ecfa5c969b
강석현 <tjrgus0713@gmail.com>
2022-06-06 01:42:17 +0900

    ⚡️:: (#23) 공지사항 api 연결

--- src/components/admin/noticeList.tsx
+++ src/components/admin/noticeList.tsx
@@ -4,21 +4,19 @@ import NoticeForm from '../common/form/noticeForm';
 import { sortOptions } from '../common/select/dropdown/options';
 import { INoticeListResponse } from '../../models/notice/response';
 import { getNoticeList } from '../../utils/api/notice';
+import { useRecoilState } from 'recoil';
+import { adminPageNationState } from '../../store/admin';
 
-interface IProps {
-  page: number;
-  setTotalElementsCount: (page: number) => void;
-}
-
-const Notification: React.FC<IProps> = ({ page, setTotalElementsCount }) => {
+const Notification: React.FC = () => {
   const [sort, setSort] = useState(sortOptions[0].option);
   const [noticeList, setNoticeList] = useState<INoticeListResponse>();
+  const [adminPageNation, setAdminPageNation] = useRecoilState(adminPageNationState);
   useEffect(() => {
     getNoticeList().then(res => {
       setNoticeList(res);
-      setTotalElementsCount(res.total_count);
+      setAdminPageNation({ ...adminPageNation, totalElement: res.total_count });
     });
-  }, [sort, page]);
+  }, [sort, adminPageNation.page]);
   return (
     <section className="noticeListWrapper">
       <DropDown value={sort} onChangeValue={setSort} options={sortOptions} />
