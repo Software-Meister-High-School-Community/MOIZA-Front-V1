8786bf77ede9bd4ac907e384b9b9574dc9dd7a31
강석현 <tjrgus0713@gmail.com>
2022-06-05 14:56:04 +0900

    ⚡️:: (#23) profile 컴퍼넌트에 id 값 넘겨주게 추가

--- src/components/mypage/index.tsx
+++ src/components/mypage/index.tsx
@@ -1,23 +1,34 @@
 import React, { useState } from 'react';
-import * as S from './styles';
 import Profile from './profile/index';
 import PostList from './postList/index';
 import PageNation from '../common/pagenation';
 import { IGetMyPageResponse } from '../../models/users/response';
+import styled from 'styled-components';
 
-const MyPageComponent: React.FC = () => {
+interface IProps {
+  id: number;
+}
+
+const ProfileComponent: React.FC<IProps> = ({ id }) => {
   const [isMine, setIsMine] = useState(false);
   const [page, setPage] = useState(1);
   const [profileContent, setProfileContent] = useState<IGetMyPageResponse>();
   return (
-    <S.Wrapper>
-      <S.UserColorBox color={'#FFB500'} />
-      <Profile isMine={isMine} />
+    <Wrapper>
+      <Profile id={id} isMine={isMine} />
       <PostList isMine={isMine} page={page} />
       <nav className="pageNation">
         <PageNation total={5} page={page} limit={1} setPage={setPage} />
       </nav>
-    </S.Wrapper>
+    </Wrapper>
   );
 };
-export default MyPageComponent;
+export default ProfileComponent;
+
+const Wrapper = styled.section`
+  width: 1200px;
+  margin: 0 auto;
+  > .pageNation {
+    margin: 134px 0 375px 0;
+  }
+`;
