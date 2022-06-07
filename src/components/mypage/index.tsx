706ff71938c79a064d75870c8e6dbce96853a2df
강석현 <tjrgus0713@gmail.com>
2022-06-07 15:52:10 +0900

    ♻️:: (#23) model값과 interface 값 수정

--- src/components/mypage/index.tsx
+++ src/components/mypage/index.tsx
@@ -2,17 +2,21 @@ import React, { useState } from 'react';
 import Profile from './profile/index';
 import PostList from './postList/index';
 import PageNation from '../common/pagenation';
-import { IGetMyPageResponse } from '../../models/users/response';
+import { IGetUserProfileResponse } from '../../models/users/response';
 import styled from 'styled-components';
 
 interface IProps {
   id: number;
 }
 
+interface IProfileContent extends IGetUserProfileResponse {
+  user_id?: number;
+}
+
 const ProfileComponent: React.FC<IProps> = ({ id }) => {
   const [isMine, setIsMine] = useState(false);
   const [page, setPage] = useState(1);
-  const [profileContent, setProfileContent] = useState<IGetMyPageResponse>();
+  const [profileContent, setProfileContent] = useState<IProfileContent>();
   return (
     <Wrapper>
       <Profile id={id} isMine={isMine} />
