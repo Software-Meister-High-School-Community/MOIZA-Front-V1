066462a420b93d84451fa38e4583addca4c7761b
강석현 <tjrgus0713@gmail.com>
2022-06-10 10:39:49 +0900

    ♻️:: (#33) pages에서 profileContent를 넘겨주게 변경

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,16 +1,38 @@
 import WithAuthorization from '../../hoc/withAuthorization';
 import ProfileComponent from '../../components/mypage';
 import { useNavigate, useParams } from 'react-router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { getUserProfile } from '../../utils/api/users';
+import { IGetUserProfileResponse } from '../../models/users/response';
+import { TBackGroundColor, TSchool, TUser } from '../../models/common';
 
 const ProfilePage = () => {
+  const [profileContent, setProfileContent] = useState<IGetUserProfileResponse>({
+    name: '다른유저',
+    school: 'DSM',
+    user_scope: 'USER',
+    profile_image_url: '',
+    profile_background_color: '#FFB500',
+    introduce: '안녕',
+    link_url: null,
+    feed_count: 0,
+    follower_count: 0,
+    following_count: 0,
+  });
   const { userId } = useParams();
   const id = Number(userId);
   const navigate = useNavigate();
   useEffect(() => {
     if (isNaN(id)) navigate('/404');
+    getUserProfile(id)
+      .then(res => setProfileContent(res))
+      .catch(err => {
+        console.log(err);
+        // todo
+        // 에러코드가 404일 경우 404페이지로 이동시킬예정
+      });
   }, [id]);
-  if (id) return <ProfileComponent id={id} />;
+  if (id) return <ProfileComponent profileContent={profileContent} id={id} />;
   return <></>;
 };
 // export default WithAuthorization(MyPage);
