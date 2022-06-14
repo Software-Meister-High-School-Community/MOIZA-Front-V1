d92b7b60d65bbec248a008a628cc3f148d7d0e79
강석현 <tjrgus0713@gmail.com>
2022-06-15 08:40:31 +0900

    ⚰️:: (#46) 필요없는 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React, { useMemo } from 'react';
 import './App.css';
 import Router from './router';
-import { isAndroid, isIOS } from 'react-device-detect';
+import { isMobile } from 'react-device-detect';
 import IsMobileAccess from './pages/isMobileAccess';
 
 function App() {
-  const isMobile = true;
   const router = useMemo(() => {
     if (isMobile) {
       return <IsMobileAccess />;
     } else {
       return <Router />;
     }
-  }, [isMobile, isAndroid, isIOS]);
+  }, [isMobile]);
   return router;
 }
 
