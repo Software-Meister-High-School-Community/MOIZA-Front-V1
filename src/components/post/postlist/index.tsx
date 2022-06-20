e4aab42ca150833902def00fb85c38c5b93c49f4
강석현 <tjrgus0713@gmail.com>
2022-06-20 23:26:48 +0900

    ⚡️:: (#63) 분야에 맞게 카테고리 보여주게 수정

--- src/components/post/postlist/index.tsx
+++ src/components/post/postlist/index.tsx
@@ -1,21 +1,42 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import * as S from './style';
 import Vector from '../../../assets/img/post/vector.svg';
 import Write from '../../../assets/img/common/writePen.svg';
 import Path from '../../common/path';
-import { postListPathArr } from '../constant';
+import { categoryList } from '../constant';
 import Dropdown from '../../common/select/dropdown';
 import RadioButton from '../../common/select/radioButton';
 import { typeArr } from '../constant';
 import PostForm from '../../common/form/postForm';
 import PagiNation from '../../common/pagenation';
 import { sortOptions } from '../../common/select/dropdown/options';
+import { TCategory } from '../../../models/common';
+import { PathType } from '../../../utils/interface/common';
 
-const PostList: React.FC = () => {
+interface IProps {
+  categoryType: TCategory;
+}
+
+const PostList: React.FC<IProps> = ({ categoryType }) => {
   const [value, setValue] = useState(sortOptions[0].option);
   const [seleted, setSeleted] = useState('all');
   const [pagenation, setPagenation] = useState(1);
-
+  const categoryName = useMemo(() => {
+    const index = categoryList.findIndex(item => item.id === categoryType);
+    return categoryList[index].summary;
+  }, [categoryType]);
+  const pathArray: PathType[] = useMemo(() => {
+    return [
+      {
+        path: '카테고리',
+        link: `/category`,
+      },
+      {
+        path: categoryType,
+        link: '',
+      },
+    ];
+  }, [categoryType]);
   return (
     <>
       <S.WriteBtn>
@@ -23,9 +44,9 @@ const PostList: React.FC = () => {
       </S.WriteBtn>
       <S.Wrapper>
         <S.PostHeadDiv>
-          <Path pathArray={postListPathArr} />
+          <Path pathArray={pathArray} />
           <S.PostNameDiv>
-            <S.PostName>OOO 커뮤니티</S.PostName>
+            <S.PostName>{categoryName} 커뮤니티</S.PostName>
             <S.PostVector src={Vector} alt="" />
           </S.PostNameDiv>
         </S.PostHeadDiv>
