ce06db67a89a96393b564fb0ee8b709c1dda4dfe
강석현 <tjrgus0713@gmail.com>
2022-06-20 23:25:56 +0900

    ⚡️:: (#63) Link 추가

--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import * as S from './style';
 import human1 from '../../assets/img/category/human1.svg';
 import human2 from '../../assets/img/category/human2.svg';
-import default1 from '../../assets/img/category/default1.svg';
-import default2 from '../../assets/img/category/default2.svg';
-import default3 from '../../assets/img/category/default3.svg';
+import { categoryList } from './constant';
+import { Link } from 'react-router-dom';
 
 const Category: React.FC = () => {
   return (
@@ -17,25 +16,21 @@ const Category: React.FC = () => {
 
         <S.Img src={human1} alt="" top="180px" left="1200px" />
         <S.Img src={human2} alt="" top="375px" left="-20px" />
-
-        <S.Item>Front-End</S.Item>
-        <S.Item>Back-End</S.Item>
-        <S.Item>
-          <S.ItemImg src={default1} alt="" />
-        </S.Item>
-        <S.Item>IOS</S.Item>
-        <S.Item>Android</S.Item>
-        <S.Item>Design</S.Item>
-        <S.Item>Game</S.Item>
-        <S.Item>
-          <S.ItemImg src={default2} alt="" />
-        </S.Item>
-        <S.Item>Security</S.Item>
-        <S.Item>Embedded</S.Item>
-        <S.Item>AI</S.Item>
-        <S.Item>
-          <S.ItemImg src={default3} alt="" />
-        </S.Item>
+        {categoryList.map((item, index) => {
+          if (index === 2 || index === 7 || index === 11)
+            return (
+              <Link to={`/category/${item.toUpperCase()}`}>
+                <S.Item>
+                  <S.ItemImg src={item} />
+                </S.Item>
+              </Link>
+            );
+          return (
+            <Link to={`/category/${item.toUpperCase()}`}>
+              <S.Item>{item}</S.Item>
+            </Link>
+          );
+        })}
       </S.Container>
     </S.Wrapper>
   );
