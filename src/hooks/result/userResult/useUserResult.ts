dcd99bbe9e396886d45a49de8f1d907315da376e
강석현 <tjrgus0713@gmail.com>
2022-06-24 11:43:52 +0900

    ⚰️:: (#69) 필요없는 코드 삭제

--- src/hooks/result/userResult/useUserResult.ts
+++ src/hooks/result/userResult/useUserResult.ts
@@ -1,44 +1,22 @@
-import React from 'react'
-import Slider from "react-slick";
+import React from 'react';
+import Slider from 'react-slick';
 
 const useUserResult = () => {
-    const slider = React.useRef<Slider>(null);
-    const users = [
-        { type: "s", name: "강석현", schoolName: "대구소프트웨어마이스터고" },
-        { type: "s", name: "정우재", schoolName: "대구소프트웨어마이스터고" },
-        { type: "s", name: "전영준", schoolName: "대구소프트웨어마이스터고" },
-        { type: "s", name: "임동현", schoolName: "대구소프트웨어마이스터고" },
-        { type: "s", name: "정우재", schoolName: "대구소프트웨어마이스터고" },
-        { type: "s", name: "장정원", schoolName: "대구소프트웨어마이스터고" },
-        { type: "s", name: "김상은", schoolName: "대구소프트웨어마이스터고" },
-    ];
+  const slider = React.useRef<Slider>(null);
 
-    const slidesToShow = users.length < 7 ? users.length : 6;
+  const handleNext = () => {
+    slider?.current?.slickNext();
+  };
 
-    const sliderSettings = {
-        speed: 500,
-        slidesToShow: slidesToShow,
-        slidesToScroll: 1,
-        infinite: true,
-        arrows: false,
-        initialSlide: 0,
-    };
+  const handlePrevious = () => {
+    slider.current?.slickPrev();
+  };
 
-    const handleNext = () => {
-        slider?.current?.slickNext();
-    };
+  return {
+    slider,
+    handleNext,
+    handlePrevious,
+  };
+};
 
-    const handlePrevious = () => {
-        slider.current?.slickPrev();
-    };
-
-    return {
-        slider,
-        users,
-        sliderSettings,
-        handleNext,
-        handlePrevious
-    }
-}
-
-export default useUserResult
+export default useUserResult;
