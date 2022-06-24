07f6230178620e24758c153e208b935e4819d3d1
강석현 <tjrgus0713@gmail.com>
2022-06-24 11:44:44 +0900

    ⚡️:: (#69) 검색 결과 관리하는 state 추가 및 더미값 추가

--- src/hooks/result/useResult.ts
+++ src/hooks/result/useResult.ts
@@ -5,6 +5,7 @@ import { useRecoilValue } from 'recoil';
 import { SearchKeywordState } from '../../store/search/keyword';
 import { IGetSearchFeedResponse } from '../../models/feeds/response';
 import { searchUser } from '../../utils/api/users';
+import { IUserSearchResponse } from '../../models/users/response';
 
 interface IRadio {
   id: TFeed;
@@ -21,7 +22,18 @@ const useResult = () => {
   const [selectedRadio, setSelectedRadio] = useState<TFeed>('ALL');
   const [categoryOption, setCategoryOption] = useState<TCategory>('ALL');
   const [sortOption, setSortOption] = useState<TSort>('LATEST');
-  const [searchResults, setSearchResults] = useState<IGetSearchFeedResponse>();
+  const [feedResults, setFeedResults] = useState<IGetSearchFeedResponse>();
+  const [userResults, setUserResults] = useState<IUserSearchResponse>({
+    user_list: [
+      { user_id: 1, name: '강석현', profile_image_url: '', user_scope: 'STUDENT', school: 'DSM' },
+      { user_id: 2, name: '전영준', profile_image_url: '', user_scope: 'GRADUATE', school: 'GSM' },
+      { user_id: 3, name: '정우재', profile_image_url: '', user_scope: 'USER', school: 'DGSW' },
+      { user_id: 4, name: '임동현', profile_image_url: '', user_scope: 'STUDENT', school: 'BSSM' },
+      { user_id: 5, name: '장정원', profile_image_url: '', user_scope: 'GRADUATE', school: 'NCMM' },
+      { user_id: 6, name: '김범진', profile_image_url: '', user_scope: 'GRADUATE', school: 'NCMM' },
+      { user_id: 7, name: '임세현', profile_image_url: '', user_scope: 'USER', school: 'DSM' },
+    ],
+  });
   const onChangeRadioValue = (value: string) => {
     const radioValue = value as TFeed;
     setSelectedRadio(radioValue);
@@ -35,12 +47,14 @@ const useResult = () => {
     setSortOption(sortOption);
   };
   const searchKeyword = useRecoilValue(SearchKeywordState);
-  useEffect(() => {
-    searchFeed(searchKeyword, categoryOption, selectedRadio, sortOption, 0);
-  }, [selectedRadio, categoryOption, sortOption, searchKeyword]);
-  useEffect(() => {
-    searchUser(searchKeyword, 0);
-  }, [searchKeyword]);
+  // useEffect(() => {
+  //   searchFeed(searchKeyword, categoryOption, selectedRadio, sortOption, 0).then(res =>
+  //     setFeedResults(res),
+  //   );
+  // }, [selectedRadio, categoryOption, sortOption, searchKeyword]);
+  // useEffect(() => {
+  //   searchUser(searchKeyword, 0).then(res => setUserResults(res));
+  // }, [searchKeyword]);
   return {
     selectedRadio,
     onChangeRadioValue,
@@ -51,6 +65,8 @@ const useResult = () => {
     radios,
     onChangeCategoryOption,
     onChangeSortOption,
+    userResults,
+    feedResults,
   };
 };
 
