6f30aefcf19764279fc2a603f3c71490eb4c0fa3
강석현 <tjrgus0713@gmail.com>
2022-06-24 13:46:33 +0900

    ⚡️:: (#69) 더미값 추가

--- src/hooks/result/useResult.ts
+++ src/hooks/result/useResult.ts
@@ -22,7 +22,55 @@ const useResult = () => {
   const [selectedRadio, setSelectedRadio] = useState<TFeed>('ALL');
   const [categoryOption, setCategoryOption] = useState<TCategory>('ALL');
   const [sortOption, setSortOption] = useState<TSort>('LATEST');
-  const [feedResults, setFeedResults] = useState<IGetSearchFeedResponse>();
+  const [feedResults, setFeedResults] = useState<IGetSearchFeedResponse>({
+    total_page: 5,
+    feed_list: [
+      {
+        id: 1,
+        title: '게시글',
+        type: 'QUESTION',
+        created_at: '2022-06-23T12:50:30',
+        author_name: '강석현',
+        is_like: false,
+        view_count: 1000,
+        like_count: 50,
+        comment_count: 10,
+      },
+      {
+        id: 2,
+        title: '이거 뭐져ㅛ',
+        type: 'QUESTION',
+        created_at: '2022-06-23T15:50:30',
+        author_name: '강석현',
+        is_like: false,
+        view_count: 3000,
+        like_count: 50,
+        comment_count: 10,
+      },
+      {
+        id: 3,
+        title: '안녕하세요',
+        type: 'COMMON',
+        created_at: '2022-06-24T10:50:30',
+        author_name: '강석현',
+        is_like: false,
+        view_count: 10200,
+        like_count: 30,
+        comment_count: 10,
+      },
+      {
+        id: 4,
+        title: '반갑습니다',
+        type: 'QUESTION',
+        created_at: '2022-07-23T12:50:30',
+        author_name: '강석현',
+        is_like: false,
+        view_count: 1000,
+        like_count: 50,
+        comment_count: 103,
+      },
+    ],
+  });
   const [userResults, setUserResults] = useState<IUserSearchResponse>({
     user_list: [
       { user_id: 1, name: '강석현', profile_image_url: '', user_scope: 'STUDENT', school: 'DSM' },
