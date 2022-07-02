738b20bb7cd904886fe485b261818e061c1db03a
Yeong joon <bluehome8626@naver.com>
2022-07-02 20:50:25 +0900

    ⚡:: (#71) 불필요한 page 삭제

--- src/pages/post/PostListPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PostList from '../../components/post/postlist';
-
-const PostListPage = () => {
-  return <PostList categoryName="" categoryType="ALL" />;
-};
-
-export default PostListPage;
