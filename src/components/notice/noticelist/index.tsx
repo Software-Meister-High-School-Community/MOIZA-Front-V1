960e591cfb2fa78158fd8ba7fc5ccb0baf9673ca
Yeong joon <bluehome8626@naver.com>
2022-07-08 09:53:01 +0900

    ⚡:: (#73) Notice List 더미 추가

--- src/components/notice/noticelist/index.tsx
+++ src/components/notice/noticelist/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './style';
 import Path from '../../common/path';
 import { noticeListPathArr } from '../constant';
@@ -7,11 +7,67 @@ import PagiNation from '../../common/pagenation';
 import { sortOptions } from '../../common/select/dropdown/options';
 import { INoticeListResponse } from '../../../models/notice/response';
 import NoticeForm from '../../common/form/noticeForm';
+import { INoticeListProps } from '../../../utils/interface/Notice';
+import { Link } from 'react-router-dom';
+
+// 더미데이터 끝나고 Response로 바꾸기
 
 const NoticeList: React.FC = () => {
   const [value, setValue] = useState(sortOptions[0].option);
   const [pagenation, setPagenation] = useState(1);
-  const [noticeList, setNoticeList] = useState<INoticeListResponse>();
+  const [noticeList, setNoticeList] = useState<INoticeListProps>();
+
+  const ExampleData = {
+    totla_count: 1,
+    notice_list: [
+      {
+        title: '안녕하세요 모이자입니다.',
+        content: '내용',
+        created_at: '22/01/21  8:29',
+        is_pinned: true,
+      },
+      {
+        title: '안녕하세요 ',
+        content: '내용',
+        created_at: '22/01/21  8:29',
+        is_pinned: true,
+      },
+      {
+        title: '반가워요 여러분 저희는 모이자입니다',
+        content: '내용',
+        created_at: '22/01/21  8:29',
+        is_pinned: true,
+      },
+      {
+        title: '긴급 영구 정지 관련 공지사항',
+        content: '내용',
+        created_at: '22/01/21  8:29',
+        is_pinned: true,
+      },
+
+      {
+        title: '안녕하세요',
+        content: '내용',
+        created_at: '22/01/21  8:29',
+        is_pinned: true,
+      },
+      {
+        title: '모이자입니다 이번 해커톤의 우승팀은?',
+        content: '내용',
+        created_at: '22/01/21  8:29',
+        is_pinned: true,
+      },
+      {
+        title: '여러분 이번 공지사항은 이것입니다',
+        content: '내용',
+        created_at: '22/01/21  8:29',
+        is_pinned: true,
+      },
+    ],
+  };
+  useEffect(() => {
+    setNoticeList(ExampleData);
+  }, []);
 
   return (
     <S.Wrapper>
@@ -22,12 +78,11 @@ const NoticeList: React.FC = () => {
         </S.NoticeNameDiv>
       </S.NoticeHeadDiv>
       <S.PostDiv>
-        <S.SelectDiv>
-          <Dropdown value={value} onChangeValue={setValue} options={sortOptions} />
-        </S.SelectDiv>
         <S.Post>
           {noticeList?.notice_list.map((item, index) => (
-            <NoticeForm item={item} key={index} />
+            <Link to="/shownotice">
+              <NoticeForm item={item} key={index} />
+            </Link>
           ))}
         </S.Post>
         <nav className="pagenation">
